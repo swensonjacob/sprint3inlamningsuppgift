fe6c603983831a9d389c6fd3936d928028d459f1
stronkelicious <hugo.lindmark@gmail.com>
2019-10-23 14:14:29 +0200

    Test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -15,9 +15,6 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
 
-        // Test av hugos branch
-        // Test 2
-
     }
 
 
