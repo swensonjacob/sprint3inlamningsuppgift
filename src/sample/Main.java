a060d6b5079e3d21f6e97b88724f445e4da25eb7
”Jacob <jacob.swenson@outlook.com>
2019-10-24 22:19:46 +0200

    buttons and game space added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,12 +11,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Pussel");
+        primaryStage.setScene(new Scene(root, 500, 400));
         primaryStage.show();
 
     }
-
 
     public static void main(String[] args) {
         launch(args);
