0ed28eb3889a49970f44742b3a482ca0d22aeb5b
”Jacob <jacob.swenson@outlook.com>
2019-10-29 21:29:51 +0100

    Cleaning up code

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,125 +1,91 @@
 package sample;
 
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class Controller {
 
+    @FXML
+    private Button b1 = new Button();
+    @FXML
+    private Button b2 = new Button();
+    @FXML
+    private Button b3 = new Button();
+    @FXML
+    private Button b4 = new Button();
+    @FXML
+    private Button b5 = new Button();
+    @FXML
+    private Button b6 = new Button();
+    @FXML
+    private Button b7 = new Button();
+    @FXML
+    private Button b8 = new Button();
+    @FXML
+    private Button b9 = new Button();
+    @FXML
+    private Button b10 = new Button();
+    @FXML
+    private Button b11 = new Button();
+    @FXML
+    private Button b12 = new Button();
+    @FXML
+    private Button b13 = new Button();
+    @FXML
+    private Button b14 = new Button();
+    @FXML
+    private Button b15 = new Button();
+    @FXML
+    private Button nullButton = new Button();
+    @FXML
+    private HBox youWon = new HBox();
+    @FXML
+    private GridPane gameSpace = new GridPane();
+    @FXML
+    private Label startingDeathStar;
+    @FXML
+    private Label brokenDeathStar;
 
-    @FXML
-    Button b1 = new Button();
-    @FXML
-    Button b2 = new Button();
-    @FXML
-    Button b3 = new Button();
-    @FXML
-    Button b4 = new Button();
-    @FXML
-    Button b5 = new Button();
-    @FXML
-    Button b6 = new Button();
-    @FXML
-    Button b7 = new Button();
-    @FXML
-    Button b8 = new Button();
-    @FXML
-    Button b9 = new Button();
-    @FXML
-    Button b10 = new Button();
-    @FXML
-    Button b11 = new Button();
-    @FXML
-    Button b12 = new Button();
-    @FXML
-    Button b13 = new Button();
-    @FXML
-    Button b14 = new Button();
-    @FXML
-    Button b15 = new Button();
-    @FXML
-    Button nullButton = new Button();
-    @FXML
-    HBox youWon = new HBox();
-    @FXML
-    GridPane gameSpace = new GridPane();
-    @FXML
-    Label startingDeathStar;
-    @FXML
-    Label brokenDeathStar;
-
-    List<Button> buttons = new ArrayList<>();
+    private List<Button> buttons;
 
     public void initialize() {
-
-        addButtons(b1, b2, b3, b4, b5, b6, b7, b8);
-        addButtons(b9, b10, b11, b12, b13, b14, b15, nullButton);
-
-    }
-
-    private void addButtons(Button b1, Button b2, Button b3, Button b4, Button b5, Button b6, Button b7, Button b8) {
-        buttons.add(b1);
-        buttons.add(b2);
-        buttons.add(b3);
-        buttons.add(b4);
-        buttons.add(b5);
-        buttons.add(b6);
-        buttons.add(b7);
-        buttons.add(b8);
-
+        buttons = Arrays.asList(b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, nullButton);
     }
 
     @FXML
     public void onButtonClicked(ActionEvent e) {
         for (Button button : buttons) {
-            if (e.getSource().equals(button)) {
-                if (buttonIsChangeable(button)) {
-                    changeButtonPlace(button);
-                    if (didYouWin()) {
-                        winMessage();
-                    }
+            if (e.getSource().equals(button) && buttonIsChangeable(button)) {
+                changeButtonPlace(button);
+                if (didYouWin()) {
+                    winMessage();
                 }
             }
         }
     }
 
     @FXML
-    public void newGame(ActionEvent e) {
+    public void newGame() {
         gameSpace.setVisible(true);
         winMessageGone();
         placeButtons(buttons);
     }
 
     @FXML
-    public void exit(ActionEvent e) {
+    public void showExitDialog() {
         Dialog<ButtonType> exitDialog = new Dialog<>();
         FXMLLoader fxmlLoader = new FXMLLoader();
         fxmlLoader.setLocation(getClass().getResource("addExitDialog.fxml"));
         try {
-            exitDialog.setTitle("Exit");
             exitDialog.getDialogPane().setContent(fxmlLoader.load());
-            exitDialog.getDialogPane().getButtonTypes().addAll(ButtonType.YES,ButtonType.NO);
-            ((Button) exitDialog.getDialogPane().lookupButton(ButtonType.YES)).setText("Yes");
-            ((Button) exitDialog.getDialogPane().lookupButton(ButtonType.NO)).setText("No");
-            exitDialog.getDialogPane().getStylesheets().add(
-                    getClass().getResource("exitDialogStyle.css").toExternalForm());
-            exitDialog.getDialogPane().getStyleClass().add("exitDialogStyle");
-
+            addDialogButtons(exitDialog);
         } catch (IOException e1) {
             System.out.println("Error");
             e1.printStackTrace();
@@ -129,6 +95,14 @@ public class Controller {
         if (result.isPresent() && result.get() == ButtonType.YES) {
             System.exit(0);
         }
+    }
+
+    public void addDialogButtons(Dialog<ButtonType> exitDialog) {
+        exitDialog.getDialogPane().getButtonTypes().addAll(ButtonType.YES, ButtonType.NO);
+        ((Button) exitDialog.getDialogPane().lookupButton(ButtonType.YES)).setText("Yes");
+        ((Button) exitDialog.getDialogPane().lookupButton(ButtonType.NO)).setText("No");
+        exitDialog.getDialogPane().getStylesheets().add(getClass().getResource("exitDialogStyle.css").toExternalForm());
+        exitDialog.getDialogPane().getStyleClass().add("exitDialogStyle");
     }
 
     public void placeButtons(List<Button> shuffledList) {
@@ -144,12 +118,10 @@ public class Controller {
     }
 
     public void changeButtonPlace(Button button) {
-        int buttonColIndex = GridPane.getColumnIndex(button);
-        int buttonRowIndex = GridPane.getRowIndex(button);
         int nullColIndex = GridPane.getColumnIndex(nullButton);
         int nullRowIndex = GridPane.getRowIndex(nullButton);
-        GridPane.setColumnIndex(nullButton, buttonColIndex);
-        GridPane.setRowIndex(nullButton, buttonRowIndex);
+        GridPane.setColumnIndex(nullButton, GridPane.getColumnIndex(button));
+        GridPane.setRowIndex(nullButton, GridPane.getRowIndex(button));
         GridPane.setColumnIndex(button, nullColIndex);
         GridPane.setRowIndex(button, nullRowIndex);
     }
@@ -182,12 +154,14 @@ public class Controller {
         }
         return false;
     }
+
     public void winMessage() {
         youWon.setVisible(true);
         brokenDeathStar.setVisible(true);
         startingDeathStar.setVisible(false);
         gameSpace.setVisible(false);
     }
+
     public void winMessageGone() {
         youWon.setVisible(false);
         brokenDeathStar.setVisible(false);
