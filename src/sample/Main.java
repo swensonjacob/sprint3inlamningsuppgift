178e8f98fa2b0c03fcf43ec733df12d0dafdc1c9
”Jacob <jacob.swenson@outlook.com>
2019-10-29 21:33:11 +0100

    Cleaning up code

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,14 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("");
         primaryStage.setScene(new Scene(root, 450, 720));
         javafx.scene.text.Font.getFamilies();
         primaryStage.setResizable(false);
