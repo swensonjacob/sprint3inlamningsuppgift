9b86ced81427c49ff9e9edf740bd69627659784e
”Jacob <jacob.swenson@outlook.com>
2019-10-30 09:28:35 +0100

    Cleaning up code

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -31,7 +31,8 @@ public class Controller {
     private List<Button> buttons;
 
     public void initialize() {
-        buttons = Arrays.asList(b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, nullButton);
+        buttons = Arrays.asList(b1, b2, b3, b4, b5, b6, b7, b8,
+                b9, b10, b11, b12, b13, b14, b15, nullButton);
     }
 
     @FXML
